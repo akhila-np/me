acb726640333244868b8199a093fb4ee36f39003
akhila112 <akhilanp200@gmail.com>
2019-06-24 13:09:42 +0530

    Updates

---
+++ precache-manifest.74eb3d1039bfefe86ffa714a4f21829b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a05b41f8b0435d2111",
+    "url": "/me/static/css/main.41ec2f82.chunk.css"
+  },
+  {
+    "revision": "47a05b41f8b0435d2111",
+    "url": "/me/static/js/main.47a05b41.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8a83888141d1947e8f6faf20bd7932b1",
+    "url": "/me/index.html"
+  }
+];
